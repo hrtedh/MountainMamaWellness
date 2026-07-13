f816613ac9888950c08637ed7c55609b7472bac5
AnthonyzGit <truevisionoffical@gmail.com>
2026-07-13 19:09:07 -0400

    Fix links (include config), remove mobile Instagram icon, update home copy

---
+++ config/links.js
@@ -0,0 +1,5 @@
+const SITE_LINKS = {
+  podcast: "https://youtu.be/xMETFnLN4FY?si=ajXrRKvxwFe2mFZW",
+  zoom: "https://calendly.com/mountainmama1400/30min",
+  facebook: "https://www.facebook.com/kristy.cirami",
+};
